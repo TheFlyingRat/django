72328c042c809b93cc6c9a428c2f49d521c9cfc0
TheFlyingRat <joeyspp50@gmail.com>
2022-05-24 12:01:20 +1000

    jddjdj

--- app.py
+++ app.py
@@ -2,24 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-PRODUCTS = [
-  {
-    'name': 'Apple',
-    'amount': 4,
-    'price': 0.9,
-  },
-  {
-    'name': 'Banana',
-    'amount': 9,
-    'price': 1.2,
-  },
-  {
-    'name': 'Coconut',
-    'amount': 2,
-    'price': 2.1,
-  },
-]
+NAME = "app"
 
 @app.route('/')
 def index():
-  return render_template('index.html', products=PRODUCTS)
+  return render_template('index.html', name=NAME)
